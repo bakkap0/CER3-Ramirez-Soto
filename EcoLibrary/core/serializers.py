20455d2cc97024d3802a7afd419993c34323041a
bakkap0 <jramirezv@usm.cl>
2025-11-30 19:37:03 -0300

    Arreglo de serializer para ver en la api

--- EcoLibrary/core/serializers.py
+++ EcoLibrary/core/serializers.py
@@ -4,7 +4,8 @@ from .models import Libro
 class LibroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Libro
-        fields = ['id', 'titulo', 'autor', 'descripcion', 'categoria', 'anio_publicacion', 'imagen_url']
+       
+        fields = ['id', 'titulo', 'autor', 'descripcion', 'categoria', 'anio_publicacion', 'imagen']
 
 class LibroDetalleSerializer(LibroSerializer):
     informacion_complementaria = serializers.SerializerMethodField() 
@@ -13,10 +14,10 @@ class LibroDetalleSerializer(LibroSerializer):
         fields = LibroSerializer.Meta.fields + ['informacion_complementaria']
         
     def get_informacion_complementaria(self, obj):
-        from .utils import obtener_datos_api_externa 
+        
+        from .views import obtener_datos_api_externa 
 
         try:
             return obtener_datos_api_externa(obj.titulo)
         except Exception:
             return "Información complementaria no disponible"
-
